1f1dc4fa1b4d35c6c6fc83536cb515583bd563db
Dylan <57924557+DylanML@users.noreply.github.com~>
2023-07-14 21:29:14 -0500

    second commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!7ch+f35-j@^6d)i2(po$!i5-csxsf)5iuwtmef9ld6ixluo=y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
