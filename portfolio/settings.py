4f375c9e6b0d6ac203265551f8ab12d815316874
Dylan <57924557+DylanML@users.noreply.github.com~>
2023-07-14 23:50:59 -0500

    second version

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -32,9 +32,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['https://my-portfolio-production-8792.up.railway.app/admin/']
 
-# CSRF_TRUSTED_ORIGINS = ['https://my-portfolio-production-8792.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://*.my-portfolio-production-8792.up.railway.app/']
 
 # Application definition
 
